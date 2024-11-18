fd81a567110a2edcd77e4843cab7cecf2a07d5a1
shadow-shield <shadowgit863@gmail.com>
2024-11-18 09:49:29 -0500

    subiendo a repo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import ('./shared/components/layout/layout.component'),
+   children:[
+    {
+      path: 'dashboard',
+      loadComponent: () => import ('./pages/dashboard/dashboard.component')
+
+    },
+    {
+      path: 'perfil',
+      loadComponent: () => import ('./pages/perfil/perfil.component')
+    },
+    {
+      path:'tablas',
+      loadComponent: () => import ('./pages/tablas/tablas.component')
+    },
+    {
+      path:'',
+      redirectTo:'dashboard',
+      pathMatch:'full'
+    }
+   ]
+  },
+  {
+    path:'login',
+    loadComponent: () => import ('./pages/authentication/login/login.component')
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+
+  }
+
+];
---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import PerfilComponent from "../perfil/perfil.component";
+import TablasComponent from "../tablas/tablas.component";
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export default class DashboardComponent {
+
+}
