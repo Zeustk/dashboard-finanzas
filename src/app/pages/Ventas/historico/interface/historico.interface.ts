11161e396366d413d7fdc2740987245b53995b6b
shadow-shield <shadowgit863@gmail.com>
2024-11-19 15:02:00 -0500

    conecntando api

--- src/app/pages/Ventas/historico/interface/historico.interface.ts
+++ src/app/pages/Ventas/historico/interface/historico.interface.ts
@@ -1,4 +1,8 @@
 export interface historico{
-
+  codigo: number;
+  mes:String;
+  gastos_del_mes:number;
+  ingresos_del_mes:number;
+  monto_a_favor:number;
 
 }
--- src/app/pages/Ventas/registro-gastos/interface/registro.interface.ts
+++ src/app/pages/Ventas/registro-gastos/interface/registro.interface.ts
@@ -1,3 +1,9 @@
 export interface registro{
+  codigo: number;
+  fecha: Date;
+  hora:String
+  monto: number;
+  categoria:string;
+  provedor:String;
 
 }
