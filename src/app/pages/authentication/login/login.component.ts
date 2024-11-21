32b5bbd20b37d30ecc3fc01534c2fd0f10c92903
Zeustk <112997729+Zeustk@users.noreply.github.com>
2024-11-21 11:37:43 -0500

    Login

--- src/app/pages/authentication/login/login.component.ts
+++ src/app/pages/authentication/login/login.component.ts
@@ -3,6 +3,8 @@ import { UsuarioServicio } from '../services/usuarios.service';
 import { usuario } from './interface/usuario';
 import Swal from 'sweetalert2';
 import { FormsModule } from '@angular/forms'; // Importa FormsModule
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -11,38 +13,49 @@ import { FormsModule } from '@angular/forms'; // Importa FormsModule
   styleUrl: './login.component.scss'
 })
 export default class LoginComponent {
-  constructor(private usuarioServ:UsuarioServicio){
-    
+  constructor(private usuarioServ: UsuarioServicio, private router: Router) {
+
   }
   @Input() Usuario: usuario = {
     Email: '',
     Contrasena: ''
-  }
-  async AgregarUsuario() {
+  }
+  async BuscandoUsuario() {
 
 
-    try{
- 
-     const respuestaRegistro = await this.usuarioServ.BuscarUsuario(this.Usuario).toPromise();
-     Swal.fire({
-       text: `Mensaje ${respuestaRegistro}`,
-       confirmButtonText: 'Aceptar'
-     });
-    }catch(error){
- 
-     const errorMessage = (error as any).error;
-     Swal.fire({
-       text:  `Mensaje${errorMessage} `  ,
-       confirmButtonText: 'Aceptar'
-     });
-     return;
+    try {
+
+      const respuestaRegistro = await this.usuarioServ.BuscarUsuario(this.Usuario).toPromise();
+
+      if (respuestaRegistro!=null) {
+        console.log('hola');
+        // Si la respuesta es válida y el usuario existe, redirigir a /dashboard
+        console.log(respuestaRegistro);
+        this.router.navigate(['/dashboard']);
+      } else {
+        // Aquí puedes manejar el caso en que no se encuentra el usuario
+        Swal.fire({
+          text: 'Usuario no encontrado',
+          confirmButtonText: 'Aceptar',
+        });
+      }
+
+
+    } catch (error) {
+
+      const errorMessage = (error as any).error;
+      Swal.fire({
+        text: `Mensaje${errorMessage} `,
+        confirmButtonText: 'Aceptar'
+      });
+      return;
 
     }
-        
-     
-   
-  
- 
- 
-   }
+
+
+
+
+
+
+  }
 }
